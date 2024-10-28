0f75ed2469369a0b9fc3c9a81f444cdb597ecf8e
Italo Magno <italomagno10@gmail.com>
2024-10-27 22:28:09 -0300

    solving the problem

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  sourceString
+    .split(';')
+    .map((item) => item.trim())
+    .filter(Boolean)
+    .forEach((line) => {
+      const [key, value] = line.split(':').map((item) => {
+        const trimValue = item.trim();
+        const removeSemiColumn = trimValue.split(';').join('');
+        const removeLineBreakValue = removeSemiColumn.replace(/\r/g, '');
+
+        return removeLineBreakValue.trimEnd();
+      });
+
+      result[key] = value;
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;
